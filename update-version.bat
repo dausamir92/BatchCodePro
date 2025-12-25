0b25bd2c701124f3016b439c48d7b14a2783befb
Firdaus <firdaus@email.com>
2025-12-25 20:05:33 +0800

    1.05.251225

--- update-version.bat
+++ update-version.bat
@@ -1,36 +1,36 @@
 @echo off
-REM Simple script to update version.json with current git info
-REM Usage: Just run this script after making a git commit
+setlocal enabledelayedexpansion
+REM Simple script to update version.json from git commit info
+REM Extracts version from the latest git commit message
+REM Example: if commit message is "v1.04.251225", version will be "1.04.251225"
 
 REM Get short commit hash
 for /f %%i in ('git rev-parse --short HEAD') do set COMMIT_HASH=%%i
 
-REM Get current date
-for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (
-    set MONTH=%%a
-    set DAY=%%b
-    set YEAR=%%c
-)
-
-REM Get last 2 digits of year
-set YEAR=%YEAR:~-2%
-
-REM Create version number (format: 1.MM.DDYY)
-set VERSION=1.%MONTH%.%DAY%%YEAR%
-
 REM Get commit date
 for /f %%i in ('git log -1 --format^=%%cd --date^=short') do set COMMIT_DATE=%%i
+
+REM Get commit message (subject line)
+for /f "delims=" %%i in ('git log -1 --format^=%%s') do set COMMIT_MSG=%%i
+
+REM Extract version from commit message
+REM Remove "v" prefix if present
+set VERSION=!COMMIT_MSG!
+set VERSION=!VERSION:v=!
+set VERSION=!VERSION:V=!
 
 REM Write to version.json
 (
 echo {
-echo   "version": "%VERSION%",
-echo   "commit": "%COMMIT_HASH%",
-echo   "date": "%COMMIT_DATE%"
+echo   "version": "!VERSION!",
+echo   "commit": "!COMMIT_HASH!",
+echo   "date": "!COMMIT_DATE!"
 echo }
 ) > version.json
 
-echo ✅ version.json updated: v%VERSION% ^(%COMMIT_HASH%^)
+echo ✅ version.json updated
+echo    Version: !VERSION!
+echo    Commit:  !COMMIT_HASH!
+echo    Date:    !COMMIT_DATE!
 echo.
-echo Now run: git add version.json
-echo Then: git commit --amend --no-edit
+echo Commit message was: !COMMIT_MSG!
